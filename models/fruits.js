2f82425d3cfabed89cecc8c5c9b04151ad5e4881
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-23 11:22:04 -0400

    mongoose

--- models/fruits.js
+++ models/fruits.js
@@ -1,3 +1,17 @@
+const mongoose = require('mongoose')
+
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    color: {
+        type:String,
+        required: true
+    },
+    readyToEat: Boolean
+})
+
 
 
 
